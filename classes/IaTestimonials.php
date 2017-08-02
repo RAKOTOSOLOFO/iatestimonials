7dc3a5359c78423ae39bf9e5e562b8c730a97fc9
iandry <ian_rak@yahoo.fr>
2017-08-02 17:15:42 +0200

    ok

--- classes/IaTestimonials.php
+++ classes/IaTestimonials.php
@@ -25,23 +25,90 @@ class IaTestimonials extends Module
             Shop::setContext(Shop::CONTEXT_ALL);
 
 
+        if (!parent::install()||
+            !$this->installDb()
+        )
 
-        if (!parent::install())
+
             return false;
 
 
         return true;
     }
 
+    public function installDb(){
+
+        $sql = "CREATE TABLE IF NOT EXISTS `"._DB_PREFIX_."iatestimonials_article`(
+            `id_iatestimonials_article` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            `body` TEXT NOT NULL,
+            `date` datetime NOT NULL
+
+        )";
+
+        if (!Db::getInstance()->Execute($sql)){
+            return false;
+        }
+
+
+        return true;
+
+
+    }
+
+    public function uninstallDb(){
+
+        $sql = "DROP TABLE `"._DB_PREFIX_."iatestimonials_article`";
+
+        if (!Db::getInstance()->Execute($sql)){
+            return false;
+        }
+        return true;
+
+
+    }
+
+
     public function uninstall()
     {
 
-        if (!parent::uninstall()){
+        if (!parent::uninstall()||
+            !$this->uninstallDb()||
+            !$this->uninstallTab()
+        ){
             return false;
         }
 
         return true;
     }
 
+    public function installTab(){
+        $tab = new Tab();
+        foreach (Language::getLanguages(true) as $lang) {
 
+	           $tab->name[$lang['id_lang']] = $this->l('Testimonials');
+
+        }
+        $tab->module = $this->name;
+        $tab->id_parent = 0;
+        $tab->class_name = 'AdminTestimonials';
+
+        if(!$tab->add()){
+            return false;
+        }
+        return true;
+
+    }
+    public function uninstallTab(){
+        $moduleTab = Tab::getCollectionFromModule($this->name);
+        if(!empty($moduleTab)){
+            foreach ($moduleTab as $tab) {
+                if(!$tab->delete()){
+                    return false;
+                }
+                # code...
+            }
+        }
+        return true;
+
+    }
 }
